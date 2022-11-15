461d7bc43e50168371f59e3e1c976effc9dde2af
”0xAureoleNode“ <aureolenode@gmail.com>
2022-11-15 14:48:53 +0800

    about cleanUp

--- src/App.js
+++ src/App.js
@@ -1,36 +1,17 @@
 import React from 'react';
 
 const App = () => {
-  const [toggle, setToggle] = React.useState(true);
+  const [timer, setTimer] = React.useState(0);
 
-  const handleToggle = () => {
-    setToggle(!toggle);
-  };
-  return <Toggle toggle={toggle} onToggle={handleToggle} />;
-};
-
-const Toggle = ({ toggle, onToggle }) => {
-  const calledOnce = React.useRef(false);
-  // bez init value is false, 所以一旦掉用完之后，calledOnce be true，直接就一直处于return，不会执行接下来的函数
   React.useEffect(() => {
-    if (calledOnce.current) {
-      return;
-    }
-    if (toggle === false) {
-      console.log('I run only once if toggle is false');
-      calledOnce.current = true;
-    }
-  }, [toggle]);
-
-  return (
-    <div>
-      <button type="button" onClick={onToggle}>
-        Toggle
-      </button>
-
-      {toggle && <div>Hello React</div>}
-    </div>
-  );
+    const interval = setInterval(
+      () => setTimer((currentTimer) => currentTimer + 1),
+      1000
+    );
+    console.log(interval);
+    return () => clearInterval(interval);
+  }, [timer]);
+  return <div>{timer}</div>;
 };
 
 export default App;
